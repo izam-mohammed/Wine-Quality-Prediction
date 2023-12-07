0d78491fb043dba9831aab1202830be227d7a87b
Izam Mohammed <izamdeveloper1@gmail.com>
2023-12-07 21:54:53 +0530

    completed the modular implementation of the model training

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 
 if __name__ == '__main__':
@@ -29,9 +30,21 @@ if __name__ == '__main__':
         raise e
 
     # third stage
+    STAGE_NAME = "Data Tranformation Stage"
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataTransformationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    # forth stage
+    STAGE_NAME = "Data Training Stage"
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainerTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -2,10 +2,12 @@ from mlProject.constants import *
 from mlProject.utils.common import read_yaml, create_directories
 from mlProject.entity.config_entity import (DataIngestionConfig,
                                             DataValidationConfig,
-                                            DataTransformationConfig)
+                                            DataTransformationConfig,
+                                            ModelTrainerConfig)
 
 # udpate configuration manager
 class ConfigurationManager:
+    
     def __init__(
         self,
         config_filepath = CONFIG_FILE_PATH,
@@ -17,6 +19,7 @@ class ConfigurationManager:
         self.schema = read_yaml(schema_filepath)
 
         create_directories([self.config.artifacts_root])
+
 
 
     
@@ -33,6 +36,7 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
 
     
 
@@ -51,6 +55,8 @@ class ConfigurationManager:
 
         return data_validation_config
     
+
+
     def get_data_transformation_config(self) -> DataTransformationConfig:
         config = self.config.data_transformation
 
@@ -62,3 +68,25 @@ class ConfigurationManager:
         )
 
         return data_transformation_config
+
+
+
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.ElasticNet
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_trainer_config = ModelTrainerConfig(
+            root_dir=config.root_dir,
+            train_data_path = config.train_data_path,
+            test_data_path = config.test_data_path,
+            model_name = config.model_name,
+            alpha = params.alpha,
+            l1_ratio = params.l1_ratio,
+            target_column = schema.name
+            
+        )
+
+        return model_trainer_config
